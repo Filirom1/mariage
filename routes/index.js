b8b8b39f52818c71407368ef4e5b4acf05e0bba0
Filirom1 <filirom1@gmail.com>
2015-12-20 22:51:08 +0100

    big update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mariage Coco Roro' });
+});
+
+router.get('/logement', function(req, res, next) {
+  res.render('logement', { title: 'Logement' });
+});
+
+router.get('/horaire', function(req, res, next) {
+  res.render('horaire', { title: 'Horaire' });
 });
 
 module.exports = router;
